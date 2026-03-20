f8b3f592d9b06146e84d0114aad5c5f73fbf4b93
Miguel Gonzalez <75241939+MiguelGonzalez77@users.noreply.github.com>
2026-03-20 17:53:55 -0500

    This is not python

--- cycleLevelSim/jsrr.asm
+++ cycleLevelSim/jsrr.asm
@@ -1,6 +1,6 @@
-.ORIG x3000 # origin of starting address
-AND R0, R0, #0 # set R0 to 0
-LEA R1, ADDR # Load jump based on ADDR which will be the address of the instruction ADD R0, R0, #2
+.ORIG x3000 ; origin of starting address
+AND R0, R0, #0 ; set R0 to 0
+LEA R1, ADDR ; Load jump based on ADDR which will be the address of the instruction ADD R0, R0, #2
 JSRR R1
 TRAP x25
 ADDR ADD R0, R0, #2
